7a18db7cb4c0f0ff59113ebf4ec3c43e45ff0643
Alex_Lin <Alex_Lin@cameo.com.tw>
2016-07-04 11:57:20 +0800

    add parse_ptt_board()

--- ptt_xml_parser.py
+++ ptt_xml_parser.py
@@ -2,16 +2,49 @@
 # -*- coding: utf-8 -*-
 import sys
 import feedparser
+from datetime import datetime
 from send_notify import send_notify_mail
 
-Target_Boards = ['Key_Mou_Pad']
+AUTO_UPDATE_SECS = 3600
+BOARD_LIST = ['Key_Mou_Pad', 'HardwareSale']
+KEYWORD_LIST = []
+AUTHOR_LIST = []
 
-data = feedparser.parse('http://rss.ptt.cc/Key_Mou_Pad.xml')
-print 'Board: ' +  data['feed']['title'] + '\n' + \
-      'Url: ' + data['feed']['id'] + '\n' + \
-      'Last updated:' + data['feed']['updated'] + '\n'
+def parse_ptt_board(board):
+    now = datetime.now()
+    data = feedparser.parse('http://rss.ptt.cc/' + board + '.xml')
 
-for item in data['entries']:
-    print item['published'] + ' ' + item['id'] + '\n' + \
-          item['author'] + ' ' + item['title'] + '\n'
+    # board information
+    board_updated_time = datetime.strptime(data['feed']['updated'], '%Y-%m-%dT%H:%M:%SZ').strftime('%Y/%m/%d %H:%M:%S')
+    board_title = data['feed']['title']
+    board_url = data['feed']['id']
 
+    print 'Board: ' +  board_title + '\n' + \
+          'Url: ' + board_url + '\n' + \
+          'Last updated: ' + board_updated_time + '\n'
+
+    # article infomation
+    for item in data['entries']:
+        article_author = item['author']
+        article_title = item['title']
+        article_url = item['id']
+        article_publish_time = datetime.strptime(item['published'], '%Y-%m-%dT%H:%M:%SZ')
+
+        if (now - article_publish_time).total_seconds() < AUTO_UPDATE_SECS:
+            time_str = article_publish_time.strftime('%m/%d %H:%M:%S')
+            print '    ' + time_str + '  ' +  article_author + ' ' + article_title + '\n' + \
+                  '    ' + article_url + '\n'
+    print ''
+
+
+def main():
+    print 'Board List: ' + str(BOARD_LIST)
+    print 'Keyword:' +  str(KEYWORD_LIST)
+    print 'Author: ' + str(AUTHOR_LIST) + '\n'
+
+    for x in BOARD_LIST:
+        parse_ptt_board(x)
+
+
+if __name__ == '__main__':
+    main()
