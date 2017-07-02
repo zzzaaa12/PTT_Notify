52679d74de4fad697440b27c670ec0a433a38297
zzzaaa12 <zzzaaa12@gmail.com>
2017-07-02 19:16:59 +0800

    add specific setting for every board

--- ptt_notify.py
+++ ptt_notify.py
@@ -5,152 +5,147 @@ import sys
 import time
 import feedparser
 import requests
+import json
 from datetime import datetime
 from datetime import timedelta
 from send_notify import send_notify_mail
 
 # files in this project
 from setting import AUTO_UPDATE_SECS
-from setting import BOARD_LIST
-from setting import SHOW_ALL_BOARD
-from setting import KEYWORD_LIST
-from setting import AUTHOR_LIST
 from setting import SEND_EMAIL
+from setting import BOARDS
+from setting import SHOW_CONTENT
+from setting import G_KEYWORDS
+from setting import G_AUTHORS
 
 class PttXmlParser:
     def __init__(self):
         self.last_updated = datetime.now() + timedelta(hours = -1)
-        self.board_list = BOARD_LIST
-        self.board_data = []
-        for x in SHOW_ALL_BOARD:
-            if x not in self.board_list:
-                self.board_list.append(x)
+        self.board_list = BOARDS
+        for board in self.board_list:
+            board['last_updated'] = datetime.now() + timedelta(hours = -1)
 
-    def prepare_board_info(self):
-        for x in self.board_list:
-            # get ptt articles in the last hour at first run
-            data = {'board':'', 'last_updated': datetime.now() + timedelta(hours = -1)}
-            data['board'] = x
-            self.board_data.append(data)
 
-    def search_data(self, author, title):
-        for x in AUTHOR_LIST:
-            if author == x:
-                return True
-        for x in KEYWORD_LIST:
-            if title.lower().find(x.lower()) != -1:
-                return True
+    def get_matches(self, board_name, title, author, content):
+        for board in self.board_list:
+            if board['board_name'].lower() != board_name.lower():
+                continue
+
+            # specific keywords
+            for keyword in board['keywords']:
+                if title.lower().find(keyword.lower()) > -1:
+                    return True
+
+            # specific authors
+            for people in board['authors']:
+                if people.lower() == author.lower():
+                    return True
+
+            # general keywords
+            if board['search_general_keyword']:
+                for keyword in G_KEYWORDS:
+                    if title.lower().find(keyword.lower()) > -1:
+                        return True
+
+            # general authors
+            if board['search_general_author']:
+                for people in G_AUTHORS:
+                    if people.lower() == author.lower():
+                        return True
+
+            # search content
+            if board['search_content']:
+                for keyword in board['keywords']:
+                    if content.lower().find(keyword.lower()) > -1:
+                        return True
+                if board['search_general_keyword']:
+                    for keyword in board['keywords']:
+                        if content.lower().find(keyword.lower()) > -1:
+                            return True
+
         return False
 
 
-    def parse_ptt_article(self, board, show_all):
-        got_last_updated_time = False
-        now = datetime.now()
+    def parse_ptt_article(self):
+        for board in self.board_list:
+            r_url = 'https://www.ptt.cc/atom/' + board['board_name'] + '.xml'
+            print 'get ' + r_url
+            r = requests.get(r_url)
+            if r.status_code != 200:
+                print 'get url fail!!!'
+                return False
 
-        r = requests.get('https://www.ptt.cc/atom/' + board + '.xml')
-        if r.status_code != 200:
-            return False
+            data = feedparser.parse(r.text)
 
-        data = feedparser.parse(r.text)
+            # board information
+            # 1. title: data['feed']['title']
+            # 2. url: data['feed']['id']
 
-        # board information
-        board_title = data['feed']['title']
-        board_url = data['feed']['id']
+            # article infomation
+            board['article_list'] = []
+            got_last_updated_time = False
+            for item in data['entries']:
+                author = item['author']
+                title = item['title']
+                url = item['id']
+                content = item['content'][0]['value'].replace('<pre>', '').replace('</pre>', '')
 
-        for x in self.board_data:
-            if board == x['board']:
-                board_last_updated = x['last_updated']
-                break
+                ## format of item['published']: 2017-04-22T08:39:34+08:00
+                publish_time = datetime.strptime(item['published'], '%Y-%m-%dT%H:%M:%S+08:00')
 
-        # article infomation
-        self.article_list = []
-        for item in data['entries']:
-            author = item['author']
-            title = item['title']
-            url = item['id']
+                if (publish_time - board['last_updated']).total_seconds() > 0:
+                    # parse articles and compare
+                    if board['show_all_artciles'] or self.get_matches(board['board_name'], title, author, content):
+                        article_data = {'board':'', 'author':'', 'title':'', 'url':'', 'content':''}
+                        article_data['board'] = board
+                        article_data['author'] = author
+                        article_data['title'] = title
+                        article_data['url'] = url
+                        article_data['content'] = content
+                        article_data['time'] = publish_time.strftime('%H:%M')
+                        board['article_list'].append(article_data)
 
-            ## format of item['published']: 2017-04-22T08:39:34+08:00
-            publish_time = datetime.strptime(item['published'], '%Y-%m-%dT%H:%M:%S+08:00')
-
-            # get and save last_updated_time
-            if not got_last_updated_time:
-                last_article_time = publish_time
-                got_last_updated_time = True
-                x['last_updated'] = last_article_time
-
-            if (publish_time - board_last_updated).total_seconds() > 0:
-                # parse articles and compare
-                if show_all or self.search_data(author, title):
-                    article_data = {'board':'', 'author':'', 'title':'', 'url':''} 
-                    article_data['board'] = board
-                    article_data['author'] = author
-                    article_data['title'] = title
-                    article_data['url'] = url
-                    article_data['time'] = publish_time.strftime('%H:%M')
-                    self.article_list.append(article_data)
-
+                # read the newest article and save last_updated_time from it
+                if not got_last_updated_time:
+                    got_last_updated_time = True
+                    last_updated_time = publish_time
+            board['last_updated'] = publish_time
         return True
 
 
-    def print_list_info(self):
-        keyword_str = ''
-        for i in range(len(KEYWORD_LIST)):
-            if i > 0:
-                keyword_str = keyword_str + ', ' + "'" + KEYWORD_LIST[i] + "'"
-            elif i == 0:
-                keyword_str = "'" + KEYWORD_LIST[i] + "'"
-
-        print 'Board List (show all): ' + str(SHOW_ALL_BOARD)
-        print 'Board List: ' + str(self.board_list)
-        print 'Keyword List: ' + '[' + keyword_str + ']'
-        print 'Author List: ' + str(AUTHOR_LIST)
-
-
     def run(self):
-        self.print_list_info()
-        self.prepare_board_info()
-
         while True:
             mail_str = ''
             got_board_list = ''
+            print 'start at: ' + str(datetime.now().strftime('%m/%d %H:%M:%S\n'))
+
+            self.parse_ptt_article()
             for board in self.board_list:
-
-                # check show all article or not
-                show_all = False
-                for x in SHOW_ALL_BOARD:
-                    if str(x) == str(board):
-                        show_all = True
-                        break
-
-                # parse title of article
-                try:
-                    if not self.parse_ptt_article(board, show_all):
-                        print 'Error: Cannot get articles in board "' + board + '"'
-                except Exception as e:
-                    print e
-                    print '\tError: An exception occurred at ' + datetime.now().strftime('%m/%d %H:%M:%S')
-                    break
-
-                # check the articles that be selected
-                if len(self.article_list) == 0:
+                if len(board['article_list']) == 0:
                     continue
 
+                # create title of notify mail
                 if SEND_EMAIL:
-                    # create title of notify mail
                     mail_str = mail_str + board + u'板：\n'
-                    if got_board_list.find(board) == -1:
+
+                    # add board name in mail title
+                    if got_board_list.find(board['board_name']) == -1:
                         if len(got_board_list) == 0:
                             got_board_list = board
                         else:
                             got_board_list = got_board_list + '/' + board
-                    print '    ' + board + u'板：'
 
-                    # create content of notify mail
-                    for article in self.article_list:
+                    for article in board['article_list']:
                         mail_str = mail_str + '    ' + article['time'] + '   '  + article['author'] + ' ' + article['title'] + '\n'
-                        mail_str = mail_str + '    ' + article['url'] + '\n\n'
-                        print '        ' + article['time'] + ' '  + article['author'] + ' ' + article['title']
-                        print '        ' + article['url'] + '\n'
+                        mail_str = mail_str + '    ' + article['url']
+                        if SHOW_CONTENT:
+                            mail_str = mail_str + '\n    ' + article['content'].replace('\n', '\n    ') + '\n'
+                        mail_str = mail_str + '\n\n'
+
+                print '\n    ' + board['board_name'] + u'板：'
+                for article in board['article_list']:
+                    print '        ' + article['time'] + ' '  + article['author'] + ' ' + article['title']
+                    print '        ' + article['url']
 
             # save last updated time
             self.last_updated = datetime.now()
@@ -163,7 +158,7 @@ class PttXmlParser:
                 send_notify_mail('PTT [' + self.last_updated.strftime('%H:%M') + ']: ' + got_board_list, mail_str)
                 print 'notify mail sent (' + self.last_updated.strftime('%m/%d %H:%M') + ')'
 
-            print 'updated at ' + self.last_updated.strftime('%m/%d %H:%M:%S')
+            print '\nfinish at ' + self.last_updated.strftime('%m/%d %H:%M:%S')
             time.sleep(AUTO_UPDATE_SECS)
 
 
--- setting.py
+++ setting.py
@@ -1,7 +1,45 @@
 # -*- coding: utf-8 -*-
 
-AUTO_UPDATE_SECS = 1200
-BOARD_LIST = ['HardwareSale']
-SHOW_ALL_BOARD = ['Key_Mou_Pad']
-KEYWORD_LIST = [u'鍵盤'] # unicode
-AUTHOR_LIST = ['zzzaaa12']
+# update interval
+AUTO_UPDATE_SECS = 300
+
+# show content in article
+SHOW_CONTENT = True
+
+# send email for notify
+SEND_EMAIL = False
+
+# general keywords and authors for all board
+G_KEYWORDS = [u'程式']
+G_AUTHORS = ['SYSOP', 'PTT']
+
+# board settings
+BOARDS =  [
+    {
+        "board_name": "Stock",
+        "authors": ['chengwaye'],
+        "keywords": [u'三大法人買賣'],
+        "show_all_artciles": False,
+        "search_general_keyword": True,
+        "search_general_author": False,
+        "search_content": False,
+    },
+    {
+        "board_name": "HardwareSale",
+        "authors": [],
+        "keywords": ['SSD', u'硬碟'],
+        "show_all_artciles": False,
+        "search_general_keyword": False,
+        "search_general_author": False,
+        "search_content": False,
+    },
+    {
+        "board_name": "CompBook",
+        "authors": [],
+        "keywords": [],
+        "show_all_artciles": True,
+        "search_general_keyword": False,
+        "search_general_author": False,
+        "search_content": False,
+    }
+]
